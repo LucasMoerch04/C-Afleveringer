fbe4f44685fddb231d33783795b9f928ef8f6ada
Lucas Moerch <lucasmoerch04@icloud.com>
2024-11-07 10:01:17 +0100

    fix bugs and improve outputs

--- afleveringer/yatzy.c
+++ afleveringer/yatzy.c
@@ -1,62 +1,81 @@
+/*
+Navn: Lucas Metz Mørch
+Email: lmorch24@student.aau.dk
+Fag: IMPR
+Studieretning: Software
+*/
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MAX 5
+#define AMOUNT_OF_ROUNDS 16
 
+// Initiate prototypes
 void play_yatzi(void);
 int getAmountInput(void);
 int *roll_multiple_dice(int n);
-int check_for_number_in_roll(int *rolls, int n, int amount);
+int check_for_lower_in_roll(int *rolls, int n, int target);
 int calculate_score(int count, int value);
 void print_scoreboard(char *rounds[], int *scores);
-int check_for_other_in_roll(int *scores, int *rolls, int n, int rule);
-int find_pairs_in_roll(int *rolls, int n, int a, int top);
+int check_for_upper_in_roll(int *scores, int *rolls, int n, int round);
+int find_multiples_in_roll(int *rolls, int n, int amount, int ignore);
 int find_straights(int *rolls, int n, int start);
 int compare(const void *a, const void *b);
 int get_sum(int *array, int n);
 
 int main(void){
-    char g = 'y';
-    printf("Welcome to YATZI\n");
+    char answer = 'y';
+    printf("Welcome to YATZI!\n");
+    printf("-----------------\n");
+
+    // Runs the Yatzy game and asks to play again
     do {
         play_yatzi();
         printf("Want to play again? y/n\n");
-        scanf(" %c", &g);
-    } while (g == 'y');
+        scanf(" %c", &answer);
+    } while (answer == 'y');
 
     return 0;
 }
 
+// Function to play a game of Yatzy
 void play_yatzi(void){
-    int N = getAmountInput();
-    int *scores = malloc(16 * sizeof(int));
+    int n = getAmountInput();
     int count;
     int *rolls;
-
+    int *scores = malloc(AMOUNT_OF_ROUNDS * sizeof(int));
+    if (scores == NULL){
+        exit(EXIT_FAILURE);
+    }
+    // Array of round "names"
     char *rounds[] = {"Ones", "Twos", "Threes", "Fours", "Fives", "Sixes", "Bonus", "One pair", "Two pair", "Three of a kind", "Four of a kind", "Small straight", "Large straight", "Full house", "Chance", "Yatzy"};
     srand(time(NULL));
-    int rounds_amount = sizeof(rounds) / sizeof(rounds[0]);
 
-    printf("Printing dies:\n");
-    for (int i = 0; i<rounds_amount; i++){
-        if (i != 6){
-            printf("%s ", rounds[i]);
-            rolls = roll_multiple_dice(N);
-            qsort(rolls, N, sizeof(int), compare); 
+    printf("\nPrinting dice:\n");
+    for (int round = 0; round < AMOUNT_OF_ROUNDS; round++){
+        int target = round + 1;
+        // Rolls dice if not "Bonus round"
+        if (round != 6){
+            printf("%s ", rounds[round]);
+            rolls = roll_multiple_dice(n);
+            // sort the rolls for more efficient search
+            qsort(rolls, n, sizeof(int), compare); 
         }
-    
-        if (i < 6){
-            count = check_for_number_in_roll(rolls, N, i+1);
-            printf(" -- %d\n", calculate_score(count, i+1));
-            scores[i] = calculate_score(count, i+1);
+        // Gets scores for first six rounds (Lower)
+        if (round < 6){
+            count = check_for_lower_in_roll(rolls, n, target);
+            printf("-- %d\n", calculate_score(count, target));
+            scores[round] = calculate_score(count, target);
+        // Get scores for rest of rounds (Upper)
         } else {
-            count = check_for_other_in_roll(scores, rolls, N, i+1);
-            if (i != 6){
-                printf(" -- %d\n", count);
+            count = check_for_upper_in_roll(scores, rolls, n, target);
+            if (round != 6){
+                printf("-- %d\n", count);
             }
-            scores[i] = count;
+            scores[round] = count;
         }
         free(rolls);
         rolls = NULL;
@@ -65,16 +84,18 @@ void play_yatzi(void){
     free(scores);
 }
 
+// Function to let user input number of dice (least 5)
 int getAmountInput(void){
     int n;
     do {
-        printf("Enter number of dice (at least 5): ");
-        scanf("%d", &n);
+        printf("Enter number of dice you want to roll each round (at least 5): ");
+        scanf(" %d", &n);
     } while (n < 5);
     
     return n;
 }
 
+// Function to roll random dice
 int *roll_multiple_dice(int n){
     int *rolls = malloc(n * sizeof(int));
 
@@ -90,11 +111,12 @@ int *roll_multiple_dice(int n){
     return rolls;
 }
 
-int check_for_number_in_roll(int *rolls, int n, int amount){
+// Function to check for "Lowers" (1-6) and how many (max 5)
+int check_for_lower_in_roll(int *rolls, int n, int target){
     int count = 0, i = 0;
 
     do {
-        if (rolls[i] == amount){
+        if (rolls[i] == target){
             count++;
         }
         i++;
@@ -103,22 +125,26 @@ int check_for_number_in_roll(int *rolls, int n, int amount){
     return count;
 }
 
+// Function to return calculated score of round
 int calculate_score(int count, int value){
     return count * value;
 }
 
+// Function to print the final scoreboard
 void print_scoreboard(char *rounds[], int *scores){  
-
+    printf("\n----------------\n");
     printf("Score board:\n");
-    for (int i = 0; i < 16; i++){
+    for (int i = 0; i < AMOUNT_OF_ROUNDS; i++){
         printf("%s : %d \n", rounds[i], scores[i]);
     }
-    printf("Sum: %d\n", get_sum(scores, 16));
+    printf("\nTOTAL SCORE: %d\n", get_sum(scores, AMOUNT_OF_ROUNDS));
+    printf("----------------\n");
 }
 
-int check_for_other_in_roll(int *scores, int *rolls, int n, int rule){
+// Function to check for "Uppers" 
+int check_for_upper_in_roll(int *scores, int *rolls, int n, int round){
     int score, eyes, three_eyes, pair_eyes;
-    switch(rule){
+    switch(round){
         // Bonus
         case 7:
             if (get_sum(scores, 6) >= 63){
@@ -129,25 +155,25 @@ int check_for_other_in_roll(int *scores, int *rolls, int n, int rule){
             break;
         // One pair
         case 8:
-            score = find_pairs_in_roll(rolls, n, 2, 7) * 2;
+            score = find_multiples_in_roll(rolls, n, 2, 7) * 2;
             break;
         // Two pairs
         case 9: 
-            eyes = find_pairs_in_roll(rolls, n, 2, 7);
+            eyes = find_multiples_in_roll(rolls, n, 2, 7);
             if (eyes > 0){
-                pair_eyes = find_pairs_in_roll(rolls, n, 2, eyes);
-                score = (eyes) * 2 + (pair_eyes * 2);
-            } else{
+                pair_eyes = find_multiples_in_roll(rolls, n, 2, eyes);
+                score = (eyes + pair_eyes) * 2;
+            } else if (eyes == 0 || pair_eyes == 0){
                 score = 0;
             }
             break;
         // Three of a kind
         case 10: 
-            score = find_pairs_in_roll(rolls, n, 3, 7) * 3;
+            score = find_multiples_in_roll(rolls, n, 3, 7) * 3;
             break;
         // Four of a kind
         case 11:
-            score = find_pairs_in_roll(rolls, n, 4, 7) * 4;
+            score = find_multiples_in_roll(rolls, n, 4, 7) * 4;
             break;
         // Small straight
         case 12:
@@ -159,8 +185,8 @@ int check_for_other_in_roll(int *scores, int *rolls, int n, int rule){
             break;
         // Full house
         case 14:
-            three_eyes = find_pairs_in_roll(rolls, n, 3, 7);
-            pair_eyes = find_pairs_in_roll(rolls, n, 2, three_eyes);
+            three_eyes = find_multiples_in_roll(rolls, n, 3, 7);
+            pair_eyes = find_multiples_in_roll(rolls, n, 2, three_eyes);
             if (three_eyes == 0 || pair_eyes == 0){
                 score = 0;
             } else{
@@ -173,7 +199,7 @@ int check_for_other_in_roll(int *scores, int *rolls, int n, int rule){
             break;
         // YATZY
         case 16:
-            if (find_pairs_in_roll(rolls, n, 5, 7)){
+            if (find_multiples_in_roll(rolls, n, 5, 7)){
                 score = 50;
             } else {
                 score = 0;
@@ -183,9 +209,10 @@ int check_for_other_in_roll(int *scores, int *rolls, int n, int rule){
     return score;
 }
 
-int find_pairs_in_roll(int *rolls, int n, int a, int top){
+// Function to find multiples of a given "eyes"
+int find_multiples_in_roll(int *rolls, int n, int amount, int ignore){
     for (int j = 6; j > 0; j--){
-        if (j == top) {
+        if (j == ignore) {
             continue;
         }
         int count = 0;
@@ -193,7 +220,7 @@ int find_pairs_in_roll(int *rolls, int n, int a, int top){
             if (rolls[i] == j){
                 count++;
             }
-            if (count >= a){
+            if (count >= amount){
                 return j;
             }
         }
@@ -201,6 +228,7 @@ int find_pairs_in_roll(int *rolls, int n, int a, int top){
     return 0;
 }
 
+// Function look for Small and Large straights
 int find_straights(int *rolls, int n, int start){
     int count = 0;
     int target = 1 + start;
@@ -219,6 +247,7 @@ int find_straights(int *rolls, int n, int start){
     return 1;
 }
 
+// Function to calculate sum of integers in given array
 int get_sum(int *array, int n){
     int sum = 0;
     for (int i = 0; i < n; i++){
@@ -227,6 +256,7 @@ int get_sum(int *array, int n){
     return sum;
 }
 
+// Function to compare two numbers for the qsort()
 int compare(const void *a, const void *b) {
     return (*(int *)a - *(int *)b);
 }
